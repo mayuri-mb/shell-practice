cb380d3cdaaea1d0dd3ba85489a0e6b8d46390ab
mayuri <mayu.reddy10@gmail.com>
2026-01-29 11:22:32 +1100

    actions

--- conversation.sh
+++ conversation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-person1=ramesh
-person2=suresh
-echo "$person1:: Hello $person2, How are you?"
-echo "$person2:: Hi $person1. I am good, How about you!?"
+
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hi Ramesh. I am good, How about you!?"
