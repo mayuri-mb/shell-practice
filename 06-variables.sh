c12c91a16c245ecc51a40f27556250c3b90de48a
mayuri <mayu.reddy10@gmail.com>
2026-01-29 12:25:29 +1100

    actions

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-timestamp=$(date)
-echo "script executed at $timestamp"
+start_time=$(date +%s)
+echo "script executed at $start_time"
+sleep 10
+end_time+$(date +%s)
+total_time=(($end_time-$start_time))
+echo "script ended in:: $total_timeS
