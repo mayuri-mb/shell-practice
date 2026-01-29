29eb575158ca7cc8206aa169da6d81d4983d2d7f
mayuri <mayu.reddy10@gmail.com>
2026-01-29 12:27:28 +1100

    actions

--- 06-variables.sh
+++ 06-variables.sh
@@ -3,6 +3,6 @@
 start_time=$(date +%s)
 echo "script executed at $start_time"
 sleep 10
-end_time+$(date +%s)
+end_time=$(date +%s)
 total_time=(($end_time-$start_time))
 echo "script ended in:: $total_timeS
