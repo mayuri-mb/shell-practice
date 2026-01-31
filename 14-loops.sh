f27c2d2c8cea7075bc2440078ca22d0fc327b10c
mayuri <mayu.reddy10@gmail.com>
2026-01-31 11:31:25 +1100

    actions

---
+++ 14-loops.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+userid=$(id -u)
+logs_folder="/var/logs/shell-script"
+logs_file="/var/logs/shell-script/$0.log"
+
+if [ &userid -ne 0 ]; then
+   echo "Please run this script with root access"
+   exit 1
+fi
+
+mkdir -p $logs_folder
+
+validate() {
+    if [ $1 -ne 0 ]; then
+       echo "$2 failed"  | tee -a $logs_file
+    else
+       echo "$2 success" | tee -a $logs_file
+    fi
+}
+
+if package in $@  # sudo sh 14-logs.sh nginx mysql nodejs
+do
+  dnf install $package -y &>> $logs_file
+  validate $? "$package installing"
+done
