70076083149a63c83afa5b0ef9c5fb55e4bf7209
mayuri <mayu.reddy10@gmail.com>
2026-01-31 13:02:47 +1100

    actions

---
+++ 17-colors.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+userid=$(id -u)
+logs_folder="/var/logs/shell-script"
+logs_file="/var/logs/shell-script/$0.log"
+
+if [ $userid -ne 0 ]; then
+   echo -e "$R Please run this script with root access $N" | tee -a $logs_file
+   exit 1
+fi
+
+mkdir -p $logs_folder
+
+validate() {
+    if [ $1 -ne 0 ]; then
+       echo -e "$2 $R failed $N"  | tee -a $logs_file
+       exit 1
+    else
+       echo -e "$2 $G success $N" | tee -a $logs_file
+    fi
+}
+
+for package in $@  # sudo sh 14-logs.sh nginx mysql nodejs
+do
+    dnf list installed $package &>> $logs_file
+    if [ $? -ne 0 ]; then
+       echo "$package not installed, installing now"
+       dnf install $package -y &>> $logs_file
+       validate $? "$package installing"
+    else
+       echo -e "$package already installed, $Y skipping $N"
+    fi    
+done
