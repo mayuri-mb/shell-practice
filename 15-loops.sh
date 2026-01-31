2c9712ef650f9a67a3863229896806d3337c8c74
mayuri <mayu.reddy10@gmail.com>
2026-01-31 12:12:58 +1100

    actions

--- 15-loops.sh
+++ 15-loops.sh
@@ -23,11 +23,11 @@ validate() {
 for package in $@  # sudo sh 14-logs.sh nginx mysql nodejs
 do
   dnf list installed $package &>> $logs_file
-  if [ $package -ne 0 ] then
+  if [ $? -ne 0 ] then
     echo "$package not installed, installing now"
     dnf install $package -y &>> $logs_file
     validate $? "$package installing"
   else
-    echo "$package already installed"
+    echo "$package already installed, skipping"
   fi    
 done
