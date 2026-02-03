c4fa835a4a954cc251669d6f09ccf68023760765
mayuri <mayu.reddy10@gmail.com>
2026-02-03 19:51:59 +1100

    actions

--- 21-script1.sh
+++ 21-script1.sh
@@ -5,6 +5,5 @@ country=INDIA
 echo " I am in $country"
 echo "pid of script1 : $$"
 
-#sh 22-script2.sh  --> 1st method to call one script from another script
-
-source ./22-script2.sh -> 2nd method way to call one script from another script
+#sh 22-script2.sh   # --> 1st method to call one script from another script
+source ./22-script2.sh  #-> 2nd method way to call one script from another script
