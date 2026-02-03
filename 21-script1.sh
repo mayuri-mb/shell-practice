afc4a77ea8f8bcdf757c0a297188d39b38efc80d
mayuri <mayu.reddy10@gmail.com>
2026-02-03 19:45:23 +1100

    actions

---
+++ 21-script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+country=INDIA
+
+echo " I am in $country"
+echo "pid of script1 : $$
+
+sh 22-script.sh
---
+++ 22-script2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "I am in $country"
+echo "PID of script2: $$"
