36e43f0fe45e1e7c2bf102153751d942cce54d0e
mayuri <mayu.reddy10@gmail.com>
2026-02-05 12:24:16 +1100

    actions

---
+++ 23-delete-old-logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+R="\e[31m"
+B="\e[32m"
+Y="\e[33m"
+N="\e[34"
+
+logs_dir="/home/ec2-user/app-logs"
+logs_file="$logd_dir/$0.log"
+
+if [ ! -d $logs-dir ]; then
+   echo -e "$R $logs_dir does not exist $N"
+   exit 1
+fi
+
+files_to_delete=$(find $logs_dir -name ".log" -mtime +14)
+echo "$files_to_delete"
+
