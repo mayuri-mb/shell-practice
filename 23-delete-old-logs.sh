be6cf92821c5456d3ef801c2d114b0663b4302ac
mayuri <mayu.reddy10@gmail.com>
2026-02-05 16:06:07 +1100

    actions

--- 23-delete-old-logs.sh
+++ 23-delete-old-logs.sh
@@ -16,7 +16,7 @@ fi
 files_to_delete=$(find $logs_dir -name "*.log" -mtime +14)
 #echo "$files_to_delete"
 
-while IFS= read -r filepath
+while IFS= read -r filepath;
 do
   echo "deleting file: $filepath"
   rm -f $filepath 
