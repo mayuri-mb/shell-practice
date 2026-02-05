e9071e129e57f1bfdad3b9625a912d581f2e2181
mayuri <mayu.reddy10@gmail.com>
2026-02-05 15:42:06 +1100

    actions

--- 24-while-loop.sh
+++ 24-while-loop.sh
@@ -2,10 +2,17 @@
 
 count=1
 
-while [ $count -le 5 ]
+# while [ $count -le 5 ]
+# do
+#     echo "count is $count"
+#     sleep 1
+#     #increments the counter
+#     ((count++))
+# done
+
+
+while IFS= read -r line; 
 do
-    echo "count is $count"
-    sleep 1
-    #increments the counter
-    ((count+=1))
-done
+#process each line in here
+  echo "$line"
+done <./20.trap.sh #input which file to read
