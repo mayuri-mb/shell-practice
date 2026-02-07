a49caaec3726f31bdae6b4542d6e4b97e53acd95
mayuri <mayu.reddy10@gmail.com>
2026-02-07 18:38:40 +1100

    actions

--- 26-disk-usage.sh
+++ 26-disk-usage.sh
@@ -22,7 +22,7 @@ do
   PARTITION=$(echo $line | awk '{print $7}')
 
   if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then 
-      MESSAGE+="High Disk usage on $PARTITION: $USAGE% \n"  # + means it appends data else it will override
+      MESSAGE+="High Disk usage on $PARTITION: $USAGE% <br>"  # + means it appends data else it will override
   fi
 done <<< $DISK_USAGE   
 
